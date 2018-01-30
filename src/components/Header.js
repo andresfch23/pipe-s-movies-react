830d56e3f299aabd0693f67e9b92dd46657513b8
Andres Felipe Chavez Herrera <felipe.chavez@udea.edu.co>
2018-01-30 18:50:01 -0500

    adding styles to the application

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,29 @@
 import React, { Component } from 'react';
-import logo from '../logo.svg';
-import '../App.css';
+import logoUserFace from '../images/nav_user_face.png';
+
+
 
 class Header extends Component {
     render() {
         return (
-            <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <h1 className="App-title">Welcome to react</h1>
-          </header>
+        <header className="navbar">
+            <a href="" className="navbar__button js-navbar-button">
+                <span className="navbar__line"></span>
+                <span className="navbar__line"></span>
+                <span className="navbar__line"></span>
+            </a>
+            <a href="" className="navbar__title-link"><h1 className="navbar__title">Pipe's <span className="navbar__title--orange">Movies</span></h1></a>    
+            <div className="navbar__icon-container">
+                <a href=""><span className="navbar__icon navbar__icon--ticket"></span></a>
+                <a href=""><span className="navbar__icon navbar__icon--movie"></span></a>
+                <a href=""><span className="navbar__icon navbar__icon--play"></span></a>
+                <a href=""><span className="navbar__icon navbar__icon--film"></span></a>
+            </div>
+            <div className="navbar__user">
+                <span className="navbar__user-info">Emilia Clarke</span>
+                <img className="navbar__user-info" src={logoUserFace} alt="user face"></img>
+            </div>
+        </header>
         );
     }
 }
