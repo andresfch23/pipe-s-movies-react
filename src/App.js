4b92df1fe6b34b9041b617f5f62cab4662232583
Andres Felipe Chavez Herrera <felipe.chavez@udea.edu.co>
2018-01-31 18:29:17 -0500

    creating section main component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import Header from './components/Header.js';
+import MainSection from './components/Main-section.js'
 
 class App extends Component {
   render() {
     return (
       <div>
         <Header />
+        <MainSection />
       </div>
     );
   }
