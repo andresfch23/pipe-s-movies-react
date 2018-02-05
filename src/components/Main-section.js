49eb2f5856d703cd59c6bce1cf1638854c76ecea
Andres Felipe Chavez Herrera <felipe.chavez@udea.edu.co>
2018-02-05 17:00:20 -0500

    Creating moviesDB end points with axios

--- src/components/Main-section.js
+++ src/components/Main-section.js
@@ -2,7 +2,8 @@ import React, { Component } from 'react';
 import BackgroundImage from '../images/it.jpg';
 import AsideMenu from './Aside-menu.js';
 import InfoMovie from './Info-movie.js';
-import { requestGetGenresMovies } from '../requests/moviesDB';
+import { requestGetGenresMovies, requestSimilarMovies, requestTrailerMovies } from '../requests/moviesDB';
+import { requestFirstGenre } from '../requests/moviesDB';
 
 class MainSection extends Component {
     constructor(props) {
@@ -13,7 +14,18 @@ class MainSection extends Component {
         const data = requestGetGenresMovies().then(({ data: genres }) => {
             console.log(genres, 'aqui estan los generos');
         });
-       
+
+        const first = requestFirstGenre().then(({ data: results }) => {
+            console.log(results, 'titulos de peliculas');
+        });
+
+        const similar = requestSimilarMovies().then(({ data: movies }) => {
+            console.log(movies, 'peliculas similares');
+        });
+
+        const trailer = requestTrailerMovies().then(({ data: video }) => {
+            console.log(video, 'trailers de peliculas');
+        });
     }
 
     render() {
